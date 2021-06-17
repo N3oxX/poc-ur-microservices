6600e1d097e1cd4cf74deb0899f5950610f3fccb
David Martinez Perez <davidmn208@gmail.com>
2021-06-17 08:53:49 +0200

    Add files via upload

---
+++ scores/src/test/java/com/mtzperez/store/scores/ServiceScoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mtzperez.store.scores;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceScoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
